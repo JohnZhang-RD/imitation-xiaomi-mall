0dcb981ff7e532eeb7079efcd7ac6c054b6d2153
junhui <419297954@qq.com>
2024-09-05 17:50:01 +0800

    feat: 缓存商品类别信息

--- src/cn/francis/mall/web/controller/GoodsTypeServlet.java
+++ src/cn/francis/mall/web/controller/GoodsTypeServlet.java
@@ -5,6 +5,7 @@ import cn.francis.mall.service.GoodsTypeService;
 import cn.francis.mall.service.impl.GoodsTypeServiceImpl;
 import com.alibaba.fastjson2.JSON;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -29,12 +30,20 @@ public class GoodsTypeServlet extends BaseServlet {
     public String goodstypelist(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("application/json;charset=UTF-8");
 
+        ServletContext application = this.getServletContext();
+        String goodsTypeListApp = (String) application.getAttribute("goodsTypeListApp");
+
+        if (goodsTypeListApp != null) {
+            response.getWriter().write(goodsTypeListApp);
+            return null;
+        }
+
         GoodsTypeService goodsTypeService = new GoodsTypeServiceImpl();
         List<GoodsType> goodsTypeList = goodsTypeService.findByLevel(1);
 
         String jsonString = JSON.toJSONString(goodsTypeList);
         response.getWriter().write(jsonString);
-
+        application.setAttribute("goodsTypeListApp", jsonString);
         return null;
     }
 }
